3dc8c99f7ae2094f48185f3e8ac7a72f48f5beac
koyu <me@koyu.space>
2022-06-18 17:58:26 +0200

    Add logo and handle usernames properly

--- src/ts/username.ts
+++ src/ts/username.ts
@@ -1,10 +1,17 @@
 window.setTimeout(() => {
-    let username = localStorage.getItem("username").replace(/([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, '');
-    const emojis = JSON.parse(localStorage.getItem("emojis"));
-    emojis.forEach(emoji => {
-        username = username.replaceAll(`:${emoji.shortcode}: `, "").replaceAll(` :${emoji.shortcode}: `, "").replaceAll(`:${emoji.shortcode}:`, "").replaceAll(` :${emoji.shortcode}:`, "");
-    });
-    username = username+"%%%";
-    username = username.replaceAll(" ", "").replaceAll("%%%", "");
+    let username = localStorage.getItem("username")
+    try {
+        username = username.replace(/([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, '');
+    } catch (e) {}
+    if (username === null) {
+        username = window.location.href.split("?acc=")[1];
+    } else {
+        const emojis = JSON.parse(localStorage.getItem("emojis"));
+        emojis.forEach(emoji => {
+            username = username.replaceAll(`:${emoji.shortcode}: `, "").replaceAll(` :${emoji.shortcode}: `, "").replaceAll(`:${emoji.shortcode}:`, "").replaceAll(` :${emoji.shortcode}:`, "");
+        });
+        username = username+"%%%";
+        username = username.replaceAll(" ", "").replaceAll("%%%", "");
+    }
     document.getElementById("name").innerHTML = username;
 }, 20);
