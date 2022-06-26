57a9a2b5983d0e1105b5dc66e6b5fbc9208459a0
koyu <me@koyu.space>
2022-06-26 16:46:09 +0200

    Delete cookies on logout

--- src/ts/navigation.ts
+++ src/ts/navigation.ts
@@ -1,5 +1,6 @@
 document.getElementById("avatar").setAttribute("src", localStorage.getItem("avatar"));
 document.querySelector("#logout").addEventListener("click", (e) => {
+    document.cookie = "";
     localStorage.clear();
     window.location.href = '/';
     e.preventDefault();
