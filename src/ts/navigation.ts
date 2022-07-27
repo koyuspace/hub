d24bfd233ac1eb11db4847ea7769f8a479dfc7e2
koyu <me@koyu.space>
2022-07-27 22:40:39 +0200

    Clear cookies more efficiently

--- src/ts/navigation.ts
+++ src/ts/navigation.ts
@@ -1,6 +1,6 @@
 document.getElementById("avatar").setAttribute("src", localStorage.getItem("avatar"));
 document.querySelector("#logout").addEventListener("click", (e) => {
-    document.cookie = "";
+    document.cookie.split(";").forEach(function(c) { document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/"); });
     window.setTimeout(function() {
         localStorage.clear();
         window.location.href = '/';
